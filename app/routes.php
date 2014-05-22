f2af60924c875808f750f5da91b1009da73bb325
Yan Syafri Hidayat <yansyaf@gmail.com>
2014-05-22 17:19:22 +0700

    initial project

---
+++ app/routes.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('layout');
+});
+// Confide routes
+Route::get( 'user/create',                 'UserController@create');
+Route::post('user',                        'UserController@store');
+Route::get( 'user/login',                  'UserController@login');
+Route::post('user/login',                  'UserController@do_login');
+Route::get( 'user/confirm/{code}',         'UserController@confirm');
+Route::get( 'user/forgot_password',        'UserController@forgot_password');
+Route::post('user/forgot_password',        'UserController@do_forgot_password');
+Route::get( 'user/reset_password/{token}', 'UserController@reset_password');
+Route::post('user/reset_password',         'UserController@do_reset_password');
+Route::get( 'user/logout',                 'UserController@logout');
---
+++ app/views/layout.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+ 
+	<title>
+		@section('title')
+			Omelet :: 
+		@show
+	</title>	
+
+	@section('header')	    
+	    {{ HTML::style('packages/bootstrap/css/bootstrap.min.css') }}
+	    <!-- {{ HTML::style('css/main.css')}} -->
+	@show
+
+</head>
+ 
+<body>
+	@section('navbar')
+	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+	    <div class="container">
+	        <div class="navbar-header">
+	            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+	                <span class="sr-only">Toggle navigation</span>
+	                <span class="icon-bar"></span>
+	                <span class="icon-bar"></span>
+	                <span class="icon-bar"></span>
+	            </button>
+	            <a class="navbar-brand" href="#">Laravel-4-Omelet</a>
+	        </div>
+	    <div class="navbar-collapse collapse">
+	      <ul class="nav navbar-nav">
+	        <li class="active">{{ HTML::link('/', 'Home') }}</li>
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Secret <b class="caret"></b></a>
+	          <ul class="dropdown-menu">
+	            <li><a href="#">Secret 1</a></li>
+	            <li><a href="#">Secret 2</a></li>
+	          </ul>
+	        </li>
+	      </ul>
+	      <ul class="nav navbar-nav navbar-right">
+            @if (Auth::check())
+    		<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><strong>{{{ Auth::user()->username }}}</strong></a>
+    			<ul class="dropdown-menu">                    
+                    <li><a href="{{{ URL::to('admin') }}}">Admin Panel</a></li>
+                    <li><a href="{{{ URL::to('user') }}}">My Profile</a></li>
+                    <li><a href="{{{ URL::to('user/logout') }}}">Logout</a></li>
+                </ul>
+            </li>
+            @else
+            <li>{{ HTML::link('user/create', 'Create') }}</li>   
+            <li>{{ HTML::link('user/login', 'Login') }}</li>
+	        @endif
+	      </ul>
+	    </div><!--/.nav-collapse -->
+	  </div>
+	</div>
+	@show
+
+	<div class="container" style="margin-top: 40px;">
+		@yield('content')
+	</div>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+{{ HTML::script('packages/bootstrap/js/bootstrap.min.js') }}
+
+</body>
+
+</html>
